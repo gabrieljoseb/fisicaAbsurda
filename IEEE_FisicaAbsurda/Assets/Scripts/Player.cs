3041cc1305025f53ffa426e41c460a4e77ef0d9c
Gabriel José Ribeiro <gjose.benevides@gmail.com>
2019-07-07 05:07:42 -0300

    Tiro_Player
    falta o trajeto senoidal, por enquanto apenas coloquei a gravidade para -1.
    OBS: atira com o '1' ou mouse

---
+++ IEEE_FisicaAbsurda/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform shotSpawner;
+    private float fireRate = 0.5f;
+    private float nextFire;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject Tiro = Instantiate(bulletPrefab, shotSpawner.position, shotSpawner.rotation);
+        }
+    }
+}
---
+++ IEEE_FisicaAbsurda/Assets/Scripts/TiroPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroPlayer : MonoBehaviour
+{
+    public float speed = 10; //velocidade da bala
+    public float destroyTime = 1f; //tempo para que a bala suma
+    void Start()
+    {
+        Destroy(gameObject, destroyTime); //destruirá o objeto após o tempo de 'destroyTime'
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime); //instancia o tiro para a direita
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //se houver colisão com um game object com a tag 
+        if (collision.gameObject.tag == nome da tag)
+        {
+            Destroy(gameObject); //o projetil se auto destruirá
+        }
+    }*/
+}
