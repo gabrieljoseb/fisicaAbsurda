0ee4a18dfc78875a9f914a7e545f3c58ba4c0b91
Jvbarretods <52049178+Jvbarretods@users.noreply.github.com>
2019-07-08 19:10:42 -0300

    Movimentação do Personagem
    Personagem se move pros lados com A e D
    
    Pula com o W
    
    Adicionado as animações em ambos os movimentos

--- IEEE_FisicaAbsurda/Assets/Scripts/Player.cs
+++ IEEE_FisicaAbsurda/Assets/Scripts/Player.cs
@@ -4,22 +4,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public float jumpforce;
     public GameObject bulletPrefab;
     public Transform shotSpawner;
     private float fireRateSlow = 2f; //tiro devagar
     private float fireRateSenoidal = 1.2f; //tiro senoidal
     private float nextFire;
+    private bool facingRight = true;
+    private bool jump = false;
+    private bool noChao = false;
+    private Animator anim;
+    private Rigidbody2D rb;
+    private Transform GroundCheck;
 
     static public bool isDead = false; //Registra se o Player está morto ou não.
 
     void Start()
     {
-
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        GroundCheck = gameObject.transform.Find("GroundCheck");
     }
 
     // Update is called once per frame
     void Update()
     {
+        noChao = Physics2D.Linecast(transform.position, GroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetKey(KeyCode.W) && noChao)
+        {
+            jump = true;
+            anim.SetTrigger("Jump");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            if (facingRight)
+            {
+                Flip();
+            }
+            anim.SetBool("Walk", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            if (!facingRight)
+            {
+                Flip();
+            }
+            anim.SetBool("Walk", true);
+        }
+        else anim.SetBool("Walk", false);
+
+        
+
         if (Input.GetButtonDown("Fire1") && Time.time > nextFire) //tiro devagar
         {
             nextFire = Time.time + fireRateSlow;
@@ -34,5 +73,22 @@ public class Player : MonoBehaviour
             nextFire = Time.time + fireRateSenoidal;
             GameObject Tiro = Instantiate(bulletPrefab, shotSpawner.position, shotSpawner.rotation);
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (jump)
+        {
+            rb.AddForce(new Vector2(0, jumpforce));
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
