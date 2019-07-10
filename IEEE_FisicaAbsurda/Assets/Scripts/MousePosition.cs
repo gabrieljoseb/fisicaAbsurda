d0614748e660b36e05c54d944fa5e515e0012a14
Gabriel José Ribeiro <gjose.benevides@gmail.com>
2019-07-09 22:42:28 -0300

    atira na mira do mouse
    sprites do player sem braço colocados na unity e sala inicial feita

---
+++ IEEE_FisicaAbsurda/Assets/Scripts/MousePosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    // script para a arma seguir o cursor do mouse
+    void Update()
+    {
+        mousePosition();
+    }
+
+    void mousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.right = direction;
+    }
+}
