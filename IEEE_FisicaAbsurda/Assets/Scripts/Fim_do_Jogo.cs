9276a881d87dea55a7b5974fb964354f2419e90f
Jvbarretods <52049178+Jvbarretods@users.noreply.github.com>
2019-07-17 03:35:15 -0300

    Creditos E HUD Pedras

---
+++ IEEE_FisicaAbsurda/Assets/Scripts/Fim_do_Jogo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fim_do_Jogo : MonoBehaviour
+{
+    public GameObject Texto;
+    public GameObject Credito;
+    public GameObject HUD_Pedras;
+    public GameObject HUD_Vidas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            StartCoroutine(End());
+        }
+    }
+
+    IEnumerator End()
+    {
+        Texto.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        Credito.SetActive(true);
+        HUD_Pedras.SetActive(false);
+        HUD_Vidas.SetActive(false);
+    }
+}
--- IEEE_FisicaAbsurda/Assets/Scripts/PedrasConfig.cs
+++ IEEE_FisicaAbsurda/Assets/Scripts/PedrasConfig.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PedrasConfig : MonoBehaviour
 {
     public GameObject gradeDaFase; //Armazena a grade q impede a conclusão da fase
+    public GameObject Hud_Pedra;
 
 
     private void OnTriggerEnter2D(Collider2D collision) 
@@ -13,6 +14,7 @@ public class PedrasConfig : MonoBehaviour
         {
             gameObject.SetActive(false); //Desativa o sprite da Pedra
             gradeDaFase.SetActive(false);//Desativa a grade da fase
+            Hud_Pedra.SetActive(true);
         }
     }
 }
